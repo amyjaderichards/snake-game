1501b359589e95b73bd6701f266c094639421b7b
Amy Jade Richards <amyjaderichards@googlemail.com>
2020-04-07 21:25:08 +0000

    Basic functionality added

--- singlephase/public/js/MainScene.js
+++ singlephase/public/js/MainScene.js
@@ -1,7 +1,16 @@
-class MainScene extends Phaser.Scene {
+import Snake from './Snake.js'
+
+export default class MainScene extends Phaser.Scene {
     constructor() {
         super('MainScene');
-        
+    }
 
-    
-}
+    create() { 
+        //Create a new instance of Snake when the scene starts
+        this.snake = new Snake(this);
+    }
+
+    update(time) {
+        this.snake.update(time);
+    }
+};
